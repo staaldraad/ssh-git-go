e09275bdbe675fad57cf10be674bbb0e97887e71
Etienne Stalmans <estalmans@gmail.com>
2018-10-18 15:33:20 +0000

    working git clone

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -53,82 +54,114 @@ func main() {
 		}
 
 		log.Printf("New SSH connection from %s (%s)", sshConn.RemoteAddr(), sshConn.ClientVersion())
-		// Discard all global out-of-band Requests
-		go ssh.DiscardRequests(reqs)
+
 		// Accept all channels but we only really want "x"
-		go handleChannels(chans)
+		go handleConnection(reqs, chans)
 	}
 }
 
-func handleChannels(chans <-chan ssh.NewChannel) {
-	// Service the incoming Channel channel in go routine
-	for newChannel := range chans {
-		go handleChannel(newChannel)
-	}
-}
-func handleChannel(newChannel ssh.NewChannel) {
+func handleConnection(reqs <-chan *ssh.Request, chans <-chan ssh.NewChannel) {
 
-	if t := newChannel.ChannelType(); t != "session" {
-		newChannel.Reject(ssh.Prohibited, "neeeeeerp")
-		return
-	}
-
-	channel, requests, err := newChannel.Accept()
-	if err != nil {
-		log.Printf("Could not accept channel (%s)", err)
-		return
-	}
-
-	go func() {
-		for req := range requests {
-
-			// only going to respond to exec.
-			// and then only to git-upload-pack
-			if req.Type == "exec" {
-				if req.WantReply {
-					req.Reply(true, nil)
-				}
-				fmt.Println("exec")
-				fmt.Printf("%x\n", req.Payload)
-
-				// strip the first 2 bytes
-				// Only want the git-upload-pack
-				parts := strings.Split(string(req.Payload[4:]), " ")
-
-				// we only expect git-upload-pack repo.git
-				if len(parts) != 2 {
-					req.Reply(false, nil)
-					return
-				}
-
-				if 0 == strings.Compare(parts[0], "git-upload-pack") {
-
-					// ensure supplied path does contain dir traversal
-					p := path.Clean(parts[1])
-
-					// get rid of enclosing ''
-					p = strings.Trim(p, "'")
-
-					fullPath := path.Join(reposLocation, p)
-					log.Printf("Requesting repo: %s\n", fullPath)
-
-					cmd := exec.Command("/usr/bin/git-upload-pack", "--strict", fullPath)
-					cmd.Stdin = channel
-					cmd.Stdout = channel
-					cmd.Stderr = channel.Stderr()
-					cmd.Run()
-					channel.SendRequest("exit-status", false, nil)
-
-				} else {
-					req.Reply(false, nil)
-					return
-				}
-
-			} else {
-				if req.WantReply {
-					req.Reply(false, nil)
-				}
+	go func(reqs <-chan *ssh.Request) {
+		for r := range reqs {
+			if r.WantReply {
+				r.Reply(false, nil)
 			}
 		}
-	}()
+	}(reqs)
+
+	// Service the incoming Channel channel in go routine
+	for ch := range chans {
+		if t := ch.ChannelType(); t == "session" {
+
+			channel, requests, err := ch.Accept()
+			if err != nil {
+				log.Printf("Could not accept channel (%s)", err)
+				return
+			}
+			go handleChannel(channel, requests)
+		} else {
+			ch.Reject(ssh.Prohibited, "neeeeeerp")
+		}
+	}
+}
+func handleChannel(channel ssh.Channel, requests <-chan *ssh.Request) {
+	defer channel.Close()
+
+	for req := range requests {
+
+		// only going to respond to exec.
+		// and then only to git-upload-pack
+		if req.Type == "exec" {
+			if req.WantReply {
+				req.Reply(true, []byte{})
+			}
+
+			go func() {
+				handleExecChannel(channel, req)
+				channel.Close()
+			}()
+		} else {
+			if req.WantReply {
+				req.Reply(false, nil)
+			}
+		}
+	}
+
+}
+
+type execRequestMsg struct {
+	Command string
+}
+
+type exitStatusMsg struct {
+	Status uint32
+}
+
+func handleExecChannel(channel ssh.Channel, req *ssh.Request) {
+	fmt.Println("exec")
+
+	var msg execRequestMsg
+	ssh.Unmarshal(req.Payload, &msg)
+
+	// Only want the git-upload-pack
+	parts := strings.Split(msg.Command, " ")
+
+	// we only expect git-upload-pack repo.git
+	if len(parts) != 2 {
+		req.Reply(false, nil)
+		return
+	}
+
+	if 0 == strings.Compare(parts[0], "git-upload-pack") {
+
+		// ensure supplied path does contain dir traversal
+		p := path.Clean(parts[1])
+
+		// get rid of enclosing ''
+		p = strings.Trim(p, "'")
+
+		fullPath := path.Join(reposLocation, p)
+		log.Printf("Requesting repo: %s\n", fullPath)
+
+		res := doExec(channel, channel, channel.Stderr(), fullPath)
+
+		channel.SendRequest("exit-status", false, ssh.Marshal(&exitStatusMsg{res}))
+	}
+}
+
+func doExec(stdin io.Reader, stdout, stderr io.Writer, path string) uint32 {
+
+	cmd := exec.Command("/usr/bin/git-upload-pack", "--strict", path)
+	cmd.Dir = path
+	cmd.Env = []string{fmt.Sprintf("GIT_DIR=%s", path)}
+	cmd.Stdin = stdin
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Printf("Error occurred: %q\n", err)
+		return 1
+	}
+	return 0
 }
